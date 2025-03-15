1fa280820c9c73730e6b4a99d131255e3898dda9
Andre Ortega <andre.ortega.jg@gmail.com>
2025-03-15 10:34:49 -0600

    init project

--- progra_1_backend_project/Program.cs
+++ progra_1_backend_project/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace progra_1_backend_project
+﻿using System;
+
+namespace progra_1_backend_project
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Console.Write("HOLA MUNDO");
+            Console.ReadLine();
         }
     }
 }
